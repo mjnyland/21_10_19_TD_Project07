dc976fb9646b8d6d89d4cdce1ed9c90721be271a
mjnyland <mjnyland@gmail.com>
2021-11-07 13:50:24 -0600

    21_11_7_TD_Project07_v1.8

--- JS/app.js
+++ JS/app.js
@@ -8,6 +8,11 @@ const profileSliderOn = document.querySelector('.profile-slider-on');
 const profileSliderOff = document.querySelector('.profile-slider-off');
 const autoCompleteCont = document.querySelector('.autoCompleteCont');
 const autoCompleteLi = autoCompleteCont.children;
+const searchInput = document.querySelector('.search-input');
+const messageInput = document.querySelector('.message-input');
+
+console.log(messageInput);
+console.log(searchInput);
 
 
 
@@ -44,7 +49,7 @@ document.addEventListener('click', (e) => {
 });
 
 
-// Toggle On Off //
+// Settings //
 
 
 function emailSave() {
@@ -75,28 +80,69 @@ profileLoad();
 
 function clearLocalStorage() {
     localStorage.clear();
+    timezoneOptions.selectedIndex = 0;
     document.querySelector('.emailBox').checked = false;
     document.querySelector('.profileBox').checked = false;
 }
 
+
+let timezoneOptions = document.querySelector('.timezone-dropdown');
+
+document.addEventListener('change', (e) => {
+    var index = timezoneOptions.selectedIndex;
+    localStorage.setItem('selectedIndex', index);
+});
+
+function timezoneLoad(){
+    var savedIndex = localStorage.getItem('selectedIndex');
+    timezoneOptions.selectedIndex = savedIndex;
+}
+
+timezoneLoad();
+
+
+const sendButton = document.querySelector('.send-button');
+
+
 // Message Send //
 
-document.addEventListener('submit', () => {
-        const messageSection = document.querySelector('.message-user');
-        const messageConfirm = document.querySelector('.sent-confirmation');
-        messageConfirm.style.display = 'block';
+document.addEventListener('submit', (e) => {
+    const emptyFieldError = document.querySelector('.empty-field-error');
+        if (searchInput.value === ''){
+            e.preventDefault();
+            emptyFieldError.style.display = 'block';
+            console.log('there is an empty field');
+        } else if (messageInput.value === ''){
+            e.preventDefault();
+            emptyFieldError.style.display = 'block';
+            console.log('there is an empty field');
+        } else {
+            e.preventDefault();
+            emptyFieldError.style.display = 'none';
+            const messageSection = document.querySelector('.message-user');
+            const messageConfirm = document.querySelector('.sent-confirmation');
+            messageConfirm.style.display = 'block';
+            function hideSend() {
+                messageConfirm.style.display = 'none';
+            }
+            setTimeout(hideSend, 1000);
+            searchInput.value = '';
+            messageInput.value = '';
+        }
 });
+
+
+
+
 
 // Auto Complete //
 
 
 const memberNames = ['Victoria Chambers', 'Dale Byrd', 'Dawn Wood', 'Dan Oliver'];
-const searchInput = document.querySelector('.search-input');
 
-
-function letterCheck(x){
+function letterCheck(letter){
     const letters = [' ', 'Shift', 'Backspace', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    if (letters.includes(x)){
+    if (letters.includes(letter)){
         return true;
     } else {
         return false;
@@ -104,30 +150,41 @@ function letterCheck(x){
 }
 
 document.addEventListener('keyup', (e) => {
-    autoCompleteCont.innerHTML = '';
-    if(letterCheck(e.key) === true){        
-        for (let i = 0; i < memberNames.length; i++){
-            console.log(`Currently checking ${memberNames[i]}`);
-            var nameMatch = (memberNames[i].startsWith(searchInput.value));
-            if (searchInput.value === '') {
-                autoCompleteCont.style.display = 'none';
-            } else {
-                autoCompleteCont.style.display = 'block';
-                if (nameMatch === true) {
-                    console.log(memberNames[i]);
-                    var autoCompleteText = memberNames[i];
-                    autoCompleteCont.style.display = 'block';
-                    var li = document.createElement('LI');
-                    li.innerHTML = autoCompleteText;
-                    li.className = 'autoCompleteName';
-                    autoCompleteCont.appendChild(li);
-                    console.log(autoCompleteLi);
+    if (e.target.className === 'search-input'){
+        autoCompleteCont.innerHTML = '';
+        if(letterCheck(e.key) === true){        
+            for (let i = 0; i < memberNames.length; i++){
+                console.log(`Currently checking ${memberNames[i]}`);
+
+                function nameMatch(input){
+                    if (input === '') {
+                        return false;
+                    } else if (memberNames[i].toUpperCase().startsWith(input.toUpperCase()) === true){
+                        return true;
+                    }
+                }
+
+                console.log(nameMatch(searchInput.value));
+
+                if (nameMatch(searchInput.value) === false) {
+                    autoCompleteCont.style.display = 'none';
+                } else {
+                    if (nameMatch(searchInput.value) === true) {
+                        autoCompleteCont.style.display = 'block';
+                        console.log(memberNames[i]);
+                        var autoCompleteText = memberNames[i];
+                        autoCompleteCont.style.display = 'block';
+                        var li = document.createElement('LI');
+                        li.innerHTML = autoCompleteText;
+                        li.className = 'autoCompleteName';
+                        autoCompleteCont.appendChild(li);
+                        console.log(autoCompleteLi);
+                    }
                 }
             }
-        }
+        }   
     }
 });
-
 
 document.addEventListener('click', (e) => {
     const autoCompleteLi = autoCompleteCont.childNodes;
